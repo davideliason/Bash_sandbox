f1eeef1b6cf79b35f8299ad13c1c9a6080953d67
David Eliason <davethemaker@gmail.com>
2024-04-07 00:34:00 -0700

    add positional argument to capture the loop number and display to refreshed greeting

--- timeDisplay.sh
+++ timeDisplay.sh
@@ -5,14 +5,15 @@ echo "What's your name?: "; read firstName # input name to variable
 showTime(){  # create function that will display the time using date func
         echo "The time is `date`: you're doing a great job $firstName" 
         sleep 10  # wait for ten seconds
-        clear # clear screen
-        echo "Nice job, $1 " # stdout positional args from command line
+        
+        echo "Nice job, $1 here at number $2" # stdout positional args from command line
+        sleep 5 # pause
         clear     # clear the screen
 }
 
 # start of script
 for i in {1..100}
 do
-        showTime David # call the function a number of times.
+        showTime David $i  # call the function a number of times.
 done
 
