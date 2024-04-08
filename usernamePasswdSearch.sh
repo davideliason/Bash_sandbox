09c43de600e090c0a88b50a7ba84ff61d748e890
David Eliason <davethemaker@gmail.com>
2024-04-08 04:46:02 -0700

    successful user input

--- usernamePasswdSearch.sh
+++ usernamePasswdSearch.sh
@@ -3,4 +3,5 @@
 # simple script to obtain username value, search for it in /etc/passwd
 passwd_file=/etc/passwd # set password file value
 clear #clear screen
-echo "hello world"
+read -p "enter a username:" username
+echo $username
