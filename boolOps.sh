0931777c2f4105330fe69eeb6dd17d8826df6ba4
David Eliason <davethemaker@gmail.com>
2024-04-08 04:26:18 -0700

    created simply logic fork using boolean operators

--- boolOps.sh
+++ boolOps.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 # practicing Boolean operators
-echo "hello world"
+# echo "hello world"
+echo "pick a number, any number"; read usrNum # user input num
+
+if [ $usrNum -ge 10 ]; then
+        echo "more than ten!"
+elif [ $usrNum -lt 10 ]; then
+        echo "less than nine"
+else
+        echo "I am totally confused!"
+fi
+
+
+
+
+
+
