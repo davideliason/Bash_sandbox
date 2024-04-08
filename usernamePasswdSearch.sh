ded9af4465370a8149ed6a7bf209ab521d281a6c
David Eliason <davethemaker@gmail.com>
2024-04-08 04:50:41 -0700

    search the /etc/passwd file and echo to stdout the return status value

--- usernamePasswdSearch.sh
+++ usernamePasswdSearch.sh
@@ -4,4 +4,7 @@
 passwd_file=/etc/passwd # set password file value
 clear #clear screen
 read -p "enter a username:" username
-echo $username
+#echo $username
+grep $^username $passwd_file > /dev/null
+status=$?  # check system status
+echo "here is the status! $status"
