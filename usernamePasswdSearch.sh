48705108130846698e4c947682042ca2bfaae581
David Eliason <davethemaker@gmail.com>
2024-04-08 04:44:24 -0700

    initial setup for a script that will obtain username input and search /etc/passwd file to see if that value is a system username

---
+++ usernamePasswdSearch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash # set interpeter
+
+# simple script to obtain username value, search for it in /etc/passwd
+passwd_file=/etc/passwd # set password file value
+clear #clear screen
+echo "hello world"
